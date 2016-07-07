27d0a373bdc91a6f7a61b96c8950df148656bbd1
Taweechai Yuenyang <krumii.it@gmail.com>
2016-07-07 17:08:07 +0700

    add first components

---
+++ js/controllers.js
@@ -0,0 +1,72 @@
+angular.module('starter.controllers', [])
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+  $scope.loginData = {};
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/login.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  // Triggered in the login modal to close it
+  $scope.closeLogin = function() {
+    $scope.modal.hide();
+  };
+
+  // Open the login modal
+  $scope.login = function() {
+    $scope.modal.show();
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    console.log('Doing login', $scope.loginData);
+
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.closeLogin();
+    }, 1000);
+  };
+})
+
+.controller('PlaylistsCtrl', function($scope) {
+  $scope.playlists = [
+    { title: 'Reggae', id: 1 },
+    { title: 'Chill', id: 2 },
+    { title: 'Dubstep', id: 3 },
+    { title: 'Indie', id: 4 },
+    { title: 'Rap', id: 5 },
+    { title: 'Cowbell', id: 6 }
+  ];
+})
+.controller('PlaylistCtrl', function($scope, $stateParams) {
+})
+.controller('BrowserCtrl',function($scope, $stateParams){
+  $scope.txttatol = 0;
+  $scope.txttatolx = 0;
+  $scope.addtatol = function(txt){
+    var x = 0;
+    var y = parseInt(txt);
+    if($scope.txttatolx === 0){
+      $scope.txttatolx = x + y;
+    }else{
+      $scope.txttatolx = parseInt($scope.txttatolx) + y;
+    }
+  }
+  $scope.minustatol = function(txt){
+    var x = 1;
+    var y = parseInt(txt);
+    if($scope.txttatolx === 0){
+      if(x > y){
+        $scope.txttatolx =  y;
+      }else{
+        $scope.txttatolx =  y - x;
+      }
+    }else{
+      $scope.txttatolx = parseInt($scope.txttatolx) - y;
+    }
+  }
+})
+;
