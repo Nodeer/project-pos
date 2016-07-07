742d5ea6bb614e30c3d0b40e6c856c09b5e333c9
Taweechai Yuenyang <krumii.it@gmail.com>
2016-07-07 17:47:12 +0700

    test connect mysql

--- js/controllers.js
+++ js/controllers.js
@@ -40,6 +40,38 @@ angular.module('starter.controllers', [])
     { title: 'Rap', id: 5 },
     { title: 'Cowbell', id: 6 }
   ];
+
+  var mysql = require("mysql");
+
+  // First you need to create a connection to the db
+  var con = mysql.createConnection({
+      host: "localhost",
+      user: "jay",
+      password: "jay",
+      database: "sitepoint"
+    });
+
+  con.connect(function(err){
+    if(err){
+      console.log('Error connecting to Db');
+      return;
+    }
+    console.log('Connection established');
+  });
+
+  con.end(function(err) {
+    // The connection is terminated gracefully
+    // Ensures all previously enqueued queries are still
+    // before sending a COM_QUIT packet to the MySQL server.
+  });
+
+  con.query('SELECT * FROM employees',function(err,rows){
+      if(err) throw err;
+
+      console.log('Data received from Db:\n');
+      console.log(rows);
+    });
+
 })
 .controller('PlaylistCtrl', function($scope, $stateParams) {
 })
